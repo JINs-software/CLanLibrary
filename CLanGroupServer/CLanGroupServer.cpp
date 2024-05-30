4e21c357c2c712d546f78aa095265118b534700f
JinHur <jinh2352@gmail.com>
2024-05-30 21:16:17 +0900

    [그룹 스레드 동작 모드 추가] (1) IOCP 작업자 스레드 수신 -> 수신 정보 큐 삽입 -> 그룹 스레드에 이벤트로 알림 (2) IOCP 작업자 스레드 수신 -> 수신 정보 큐 삽입 -> 그룹 스레드가 폴링으로 수신 정보 큐 확인 (3) IOCP 작업자 스레드 수신 -> 세션 메시지 큐 삽입  -> 그룹 스레드가 폴링으로 세션 메시지 큐 확인

--- CLanGroupServer/CLanGroupServer.cpp
+++ CLanGroupServer/CLanGroupServer.cpp
@@ -1,104 +1,5 @@
 #include "CLanGroupServer.h"
 
-UINT __stdcall CLanGroupThread::SessionGroupThreadFunc(void* arg)
-{
-	CLanGroupThread* groupthread = (CLanGroupThread*)arg;
-
-	groupthread->OnStart();
-
-	if (groupthread->m_SetTlsMemPoolFlag) {
-		groupthread->m_SerialBuffPoolMgr->AllocTlsMemPool(groupthread->m_TlsMemPoolUnitCnt, groupthread->m_TlsMemPoolCapacity);
-	}
-
-	HANDLE events[2] = { groupthread->m_SessionGroupThreadStopEvent, groupthread->m_RecvEvent };
-	while (true) {
-		DWORD ret = WaitForMultipleObjects(2, events, false, INFINITE);
-		if (ret == WAIT_OBJECT_0) {
-			break;
-		}
-		else if (ret == WAIT_OBJECT_0 + 1) {
-#if defined(RECV_BUFF_QUEUE)
-			while (true) {
-				bool isEmpty = true;
-				stSessionRecvBuff recvBuff;
-				groupthread->m_RecvQueueMtx.lock();
-				if (!groupthread->m_RecvQueue.empty()) {
-					recvBuff = groupthread->m_RecvQueue.front();
-					groupthread->m_RecvQueue.pop();
-					groupthread->m_temp_PopCnt++;
-					isEmpty = false;
-				}
-				groupthread->m_RecvQueueMtx.unlock();
-
-				if (!isEmpty) {
-					groupthread->OnRecv(recvBuff.sessionID, *recvBuff.recvData);
-				}
-				else {
-					break;
-				}
-			}
-#elif defined(RECV_BUFF_LIST)
-			stRecvQueueSync blocked;
-			blocked.blocked = 1;
-			blocked.accessCnt = 0;
-			InterlockedOr((LONG*)&groupthread->m_RecvQueueSync, *(LONG*)&blocked);
-			while (groupthread->m_RecvQueueSync.accessCnt != 0);
-
-			auto iter = groupthread->m_RecvQueue.begin();
-			for (auto iter = groupthread->m_RecvQueue.begin(); iter != groupthread->m_RecvQueue.end(); iter++) {
-				stSessionRecvBuff& recvBuff = *iter;
-				groupthread->OnRecv(recvBuff.sessionID, *recvBuff.recvData);
-				groupthread->m_temp_PopCnt++;
-			}
-			groupthread->m_RecvQueue.clear();
-
-			InterlockedOr((LONG*)&groupthread->m_RecvQueueSyncTemp, *(LONG*)&blocked);
-			while (groupthread->m_RecvQueueSyncTemp.accessCnt != 0);
-
-			groupthread->m_RecvQueue.swap(groupthread->m_RecvQueueTemp);
-			InterlockedXor((LONG*)&groupthread->m_RecvQueueSync, *(LONG*)&blocked);
-			InterlockedXor((LONG*)&groupthread->m_RecvQueueSyncTemp, *(LONG*)&blocked);
-#endif
-		}
-		else {
-			DebugBreak();
-		}
-	}
-	return 0;
-}
-
-JBuffer* CLanGroupThread::GetSerialSendBuff()
-{
-	JBuffer* serialSendBuff = m_SerialBuffPoolMgr->GetTlsMemPool().AllocMem();
-	serialSendBuff->ClearBuffer();
-	return serialSendBuff;
-}
-
-void CLanGroupThread::Disconnect(uint64 sessionID) {
-	m_ClanGroupServer->Disconnect(sessionID);
-}
-bool CLanGroupThread::SendPacket(uint64 sessionID, JBuffer* sendDataPtr) {
-#if defined(ALLOC_BY_TLS_MEM_POOL)
-	return m_ClanGroupServer->SendPacket(sessionID, sendDataPtr);
-#else
-	shared_ptr<JBuffer> sptr = make_shared<JBuffer>(sendDataPtr);
-	return m_ClanGroupServer->SendPacket(sessionID, sptr);
-#endif
-}
-void CLanGroupThread::ForwardSessionGroup(SessionID sessionID, GroupID to) {
-	m_ClanGroupServer->ForwardSessionGroup(sessionID, m_GroupID, to);
-}
-void CLanGroupThread::PostMsgToThreadGroup(GroupID groupID, JBuffer& msg) {
-	m_ClanGroupServer->PostMsgToThreadGroup(groupID, msg);
-}
-
-void CLanGroupThread::Encode(BYTE randKey, USHORT payloadLen, BYTE& checkSum, BYTE* payloads) {
-	m_ClanGroupServer->Encode(randKey, payloadLen, checkSum, payloads);
-}
-bool CLanGroupThread::Decode(BYTE randKey, USHORT payloadLen, BYTE checkSum, BYTE* payloads) {
-	return m_ClanGroupServer->Decode(randKey, payloadLen, checkSum, payloads);
-}
-
 void CLanGroupServer::CreateGroup(GroupID newGroupID, CLanGroupThread* groupThread)
 {
 	if (m_GroupThreads.find(newGroupID) != m_GroupThreads.end()) {
@@ -165,27 +66,256 @@ void CLanGroupServer::OnRecv(UINT64 sessionID, JBuffer& recvBuff)
 	UINT16 groupID = m_SessionGroupID[sessionID];
 	ReleaseSRWLockShared(&m_SessionGroupIDSrwLock);
 
-	// case1) 그룹 스레드에 전달
-	// groupThread.OnRecv(sessionID, recvBuff);
-
-	// case2) 그룹 스레드가 대기하는 IOCP 큐에 Post 송신
-	//			-> GetOverlappedResult가 적절?
-
-	// case3) 그룹 간 락만 걸어 IOCP 작업자 스레드가 로직을 수행하도록 함.
-
-
-	// 이벤트 깨움 방식
-	// 1. 데이터 복사
-	std::shared_ptr<JBuffer> recvData = std::make_shared<JBuffer>(recvBuff.GetUseSize());
-#if defined(CALCULATE_TRANSACTION_PER_SECOND)
-	UINT recvCnt = RecvData(recvBuff, *recvData);
-	InterlockedAdd(&m_CalcTpsItems[RECV_TRANSACTION], recvCnt);
-	InterlockedAdd(&m_TotalTransaction[RECV_TRANSACTION], recvCnt);
-#else 
-	RecvData(recvBuff, *recvData);
-#endif
+	std::shared_ptr<JBuffer> recvData = make_shared<JBuffer>(recvBuff.GetUseSize());
+	UINT dirDeqSize = recvBuff.GetDirectDequeueSize();
+	if (dirDeqSize >= recvBuff.GetUseSize()) {
+		recvData->Enqueue(recvBuff.GetDequeueBufferPtr(), recvBuff.GetUseSize());
+	}
+	else {
+		recvData->Enqueue(recvBuff.GetDequeueBufferPtr(), dirDeqSize);
+		recvData->Enqueue(recvBuff.GetBeginBufferPtr(), recvBuff.GetUseSize() - dirDeqSize);
+	}
 
 	stSessionRecvBuff sessionRecvBuff{ sessionID, recvData };
 	m_GroupThreads[groupID]->PushRecvBuff(sessionRecvBuff);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void CLanGroupThread::PushRecvBuff(stSessionRecvBuff& recvBuff) {
+#if !defined(POLLING_SESSION_MESSAGE_QUEUE)
+#if defined(RECV_BUFF_QUEUE)
+	m_RecvQueueMtx.lock();
+	m_RecvQueue.push(recvBuff);
+	m_temp_PushCnt++;
+	m_RecvQueueMtx.unlock();
+
+	SetEvent(m_RecvEvent);
+#elif defined(RECV_BUFF_LIST)
+	stRecvQueueSync cmp, exg;
+	cmp = exg = m_RecvQueueSync;
+	cmp.blocked = 0;
+	exg.blocked = 0;
+	exg.accessCnt += 1;
+	do {
+		UINT ret = InterlockedCompareExchange((UINT*)&m_RecvQueueSync, *(UINT*)&exg, *(UINT*)&cmp);
+		stRecvQueueSync* retSync = (stRecvQueueSync*)&ret;
+		if (ret == *(UINT*)&cmp) {
+			m_RecvQueueMtx.lock();
+			m_RecvQueue.push_back(recvBuff);
+			m_temp_PushCnt++;
+			m_RecvQueueMtx.unlock();
+			InterlockedDecrement((UINT*)&m_RecvQueueSync);
+			break;
+		}
+		else if (retSync->blocked == 0) {
+			continue;
+		}
+		else {	// retSync->blocked == 1
+			bool breakFlag = false;
+			do {
+				cmp = exg = m_RecvQueueSyncTemp;
+				cmp.blocked = 0;
+				exg.blocked = 0;
+				exg.accessCnt += 1;
+				ret = InterlockedCompareExchange((UINT*)&m_RecvQueueSyncTemp, *(UINT*)&exg, *(UINT*)&cmp);
+				stRecvQueueSync* retSync = (stRecvQueueSync*)&ret;
+				if (ret == *(UINT*)&cmp) {
+					m_RecvQueueMtx.lock();
+					m_RecvQueueTemp.push_back(recvBuff);
+					m_temp_PushCnt++;
+					m_RecvQueueMtx.unlock();
+					InterlockedDecrement((UINT*)&m_RecvQueueSyncTemp);
+					breakFlag = true;
+					break;
+				}
+				else if (retSync->blocked == 0) {
+					continue;
+				}
+				else {
+					break;
+				}
+			} while (true);
+
+			if (breakFlag) {
+				break;
+			}
+		}
+	} while (true);
+
+#if defined(SETEVENT_RECEIVE_EVENT)
+	SetEvent(m_RecvEvent);
+#endif
+
+#endif
+#else 	
+	bool isPresent = true;
+	AcquireSRWLockShared(&m_SessionMsgQueueSRWLock);
+	SessionQueueMap::iterator iter = m_SessionMsgQueueMap.find(recvBuff.sessionID);
+	if (iter == m_SessionMsgQueueMap.end()) {
+		isPresent = false;
+	}
+	ReleaseSRWLockShared(&m_SessionMsgQueueSRWLock);
+
+	if (!isPresent) {
+		AcquireSRWLockExclusive(&m_SessionMsgQueueSRWLock);
+		if (m_SessionMsgQueueMap.find(recvBuff.sessionID) == m_SessionMsgQueueMap.end()) {
+			CRITICAL_SECTION* lockPtr = new CRITICAL_SECTION();
+			InitializeCriticalSection(lockPtr);
+			std::pair<SessionQueueMap::iterator, bool> ret = m_SessionMsgQueueMap.insert({ recvBuff.sessionID, std::make_pair(std::queue<shared_ptr<JBuffer>>(), lockPtr) });
+			if (!ret.second) {
+				DebugBreak();
+			}
+			iter = ret.first;
+		}
+		ReleaseSRWLockExclusive(&m_SessionMsgQueueSRWLock);
+	}
+
+	std::queue<std::shared_ptr<JBuffer>>& recvQueue = iter->second.first;
+	CRITICAL_SECTION* recvQueueLock = iter->second.second;
+	EnterCriticalSection(recvQueueLock);
+	recvQueue.push(recvBuff.recvData);
+	LeaveCriticalSection(recvQueueLock);
+#endif
+}
+
+UINT __stdcall CLanGroupThread::SessionGroupThreadFunc(void* arg)
+{
+	CLanGroupThread* groupthread = (CLanGroupThread*)arg;
+
+	groupthread->OnStart();
+
+	if (groupthread->m_SetTlsMemPoolFlag) {
+		groupthread->m_SerialBuffPoolMgr->AllocTlsMemPool(groupthread->m_TlsMemPoolUnitCnt, groupthread->m_TlsMemPoolCapacity);
+	}
+
+#if defined(SETEVENT_RECEIVE_EVENT)
+	HANDLE events[2] = { groupthread->m_SessionGroupThreadStopEvent, groupthread->m_RecvEvent };
+	while (true) {
+		DWORD ret = WaitForMultipleObjects(2, events, false, INFINITE);
+		if (ret == WAIT_OBJECT_0) {
+			break;
+		}
+		else if (ret == WAIT_OBJECT_0 + 1) {
+#if defined(RECV_BUFF_QUEUE)
+			while (true) {
+				bool isEmpty = true;
+				stSessionRecvBuff recvBuff;
+				groupthread->m_RecvQueueMtx.lock();
+				if (!groupthread->m_RecvQueue.empty()) {
+					recvBuff = groupthread->m_RecvQueue.front();
+					groupthread->m_RecvQueue.pop();
+					groupthread->m_temp_PopCnt++;
+					isEmpty = false;
+				}
+				groupthread->m_RecvQueueMtx.unlock();
+
+				if (!isEmpty) {
+					groupthread->OnRecv(recvBuff.sessionID, *recvBuff.recvData);
+				}
+				else {
+					break;
+				}
+			}
+#elif defined(RECV_BUFF_LIST)
+			stRecvQueueSync blocked;
+			blocked.blocked = 1;
+			blocked.accessCnt = 0;
+			InterlockedOr((LONG*)&groupthread->m_RecvQueueSync, *(LONG*)&blocked);
+			while (groupthread->m_RecvQueueSync.accessCnt != 0);
+
+			auto iter = groupthread->m_RecvQueue.begin();
+			for (auto iter = groupthread->m_RecvQueue.begin(); iter != groupthread->m_RecvQueue.end(); iter++) {
+				stSessionRecvBuff& recvBuff = *iter;
+				groupthread->OnMessage(recvBuff.sessionID, *recvBuff.recvData);
+				groupthread->m_temp_PopCnt++;
+			}
+			groupthread->m_RecvQueue.clear();
+
+			InterlockedOr((LONG*)&groupthread->m_RecvQueueSyncTemp, *(LONG*)&blocked);
+			while (groupthread->m_RecvQueueSyncTemp.accessCnt != 0);
+
+			groupthread->m_RecvQueue.swap(groupthread->m_RecvQueueTemp);
+			InterlockedXor((LONG*)&groupthread->m_RecvQueueSync, *(LONG*)&blocked);
+			InterlockedXor((LONG*)&groupthread->m_RecvQueueSyncTemp, *(LONG*)&blocked);
+#endif
+		}
+		else {
+			DebugBreak();
+		}
+	}
+#elif defined(POLLING_RECEIVE_EVENT)
+	while (!groupthread->m_SessionGroupThreadStopFlag) {
+#if defined(RECV_BUFF_LIST)
+		if (groupthread->m_RecvQueue.size() == 0) {
+			continue;
+		}
+
+		stRecvQueueSync blocked;
+		blocked.blocked = 1;
+		blocked.accessCnt = 0;
+		InterlockedOr((LONG*)&groupthread->m_RecvQueueSync, *(LONG*)&blocked);
+		while (groupthread->m_RecvQueueSync.accessCnt != 0);
+
+		auto iter = groupthread->m_RecvQueue.begin();
+		for (auto iter = groupthread->m_RecvQueue.begin(); iter != groupthread->m_RecvQueue.end(); iter++) {
+			stSessionRecvBuff& recvBuff = *iter;
+			groupthread->OnMessage(recvBuff.sessionID, *recvBuff.recvData);
+			groupthread->m_temp_PopCnt++;
+		}
+		groupthread->m_RecvQueue.clear();
+
+		InterlockedOr((LONG*)&groupthread->m_RecvQueueSyncTemp, *(LONG*)&blocked);
+		while (groupthread->m_RecvQueueSyncTemp.accessCnt != 0);
+
+		groupthread->m_RecvQueue.swap(groupthread->m_RecvQueueTemp);
+		InterlockedXor((LONG*)&groupthread->m_RecvQueueSync, *(LONG*)&blocked);
+		InterlockedXor((LONG*)&groupthread->m_RecvQueueSyncTemp, *(LONG*)&blocked);
+#endif
+	}
+#elif defined(POLLING_SESSION_MESSAGE_QUEUE)
+	while (!groupthread->m_SessionGroupThreadStopFlag) {
+		AcquireSRWLockShared(&groupthread->m_SessionMsgQueueSRWLock);
+		for (auto& iter : groupthread->m_SessionMsgQueueMap) {
+			SessionID sessionID = iter.first;
+			std::queue<std::shared_ptr<JBuffer>>& recvQueue = iter.second.first;
+			CRITICAL_SECTION* recvQueueLock = iter.second.second;
+
+			EnterCriticalSection(recvQueueLock);
+			while (!recvQueue.empty()) {
+				groupthread->OnMessage(sessionID, *recvQueue.front());
+				recvQueue.pop();
+			}
+			LeaveCriticalSection(recvQueueLock);
+		}
+		ReleaseSRWLockShared(&groupthread->m_SessionMsgQueueSRWLock);
+	}
+#endif
+	return 0;
+}
+
+//JBuffer* CLanGroupThread::GetSerialSendBuff()
+//{
+//	JBuffer* serialSendBuff = m_SerialBuffPoolMgr->GetTlsMemPool().AllocMem();
+//	serialSendBuff->ClearBuffer();
+//	return serialSendBuff;
+//}
+
+void CLanGroupThread::Disconnect(uint64 sessionID) {
+	m_ClanGroupServer->Disconnect(sessionID);
+}
+bool CLanGroupThread::SendPacket(uint64 sessionID, JBuffer* sendDataPtr) {
+#if defined(ALLOC_BY_TLS_MEM_POOL)
+	return m_ClanGroupServer->SendPacket(sessionID, sendDataPtr);
+#else
+	shared_ptr<JBuffer> sptr = make_shared<JBuffer>(sendDataPtr);
+	return m_ClanGroupServer->SendPacket(sessionID, sptr);
+#endif
+}
+void CLanGroupThread::ForwardSessionGroup(SessionID sessionID, GroupID to) {
+	m_ClanGroupServer->ForwardSessionGroup(sessionID, m_GroupID, to);
+}
+void CLanGroupThread::PostMsgToThreadGroup(GroupID groupID, JBuffer& msg) {
+	m_ClanGroupServer->PostMsgToThreadGroup(groupID, msg);
 }
 
--- include/CLanGroupServer.h
+++ include/CLanGroupServer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "CLanServer.h"
+#include "CLanGroupConfig.h"
 
 using SessionID = UINT64;
 using GroupID = UINT16;
@@ -9,172 +10,10 @@ struct stSessionRecvBuff {
 	std::shared_ptr<JBuffer> recvData;
 };
 
-// 전방 선언
-class CLanGroupServer;
-
 //#define RECV_BUFF_QUEUE
 #define RECV_BUFF_LIST
 
-class CLanGroupThread {
-private:
-#if defined(RECV_BUFF_QUEUE)
-	std::queue<stSessionRecvBuff>	m_RecvQueue;
-#elif defined(RECV_BUFF_LIST)
-	std::list<stSessionRecvBuff>	m_RecvQueue;
-	std::list<stSessionRecvBuff>	m_RecvQueueTemp;
-	struct stRecvQueueSync {
-		UINT accessCnt : 24;
-		UINT blocked : 8;
-	};
-	stRecvQueueSync					m_RecvQueueSync;
-	stRecvQueueSync					m_RecvQueueSyncTemp;
-#endif
-	HANDLE							m_RecvEvent;
-	UINT							m_temp_PushCnt = 0;
-	UINT							m_temp_PopCnt = 0;
-	std::mutex						m_RecvQueueMtx;
-
-	HANDLE							m_SessionGroupThread;
-	HANDLE							m_SessionGroupThreadStopEvent;
-
-	CLanGroupServer*				m_ClanGroupServer;
-
-protected:
-	GroupID							m_GroupID;
-
-#if defined(ALLOC_BY_TLS_MEM_POOL)
-	TlsMemPoolManager<JBuffer>* m_SerialBuffPoolMgr;
-	bool						m_SetTlsMemPoolFlag;
-	size_t						m_TlsMemPoolUnitCnt;
-	size_t						m_TlsMemPoolCapacity;
-#endif
-
-	UINT32						m_ProcCnt = 0;
-	
-public:
-#if defined(ALLOC_BY_TLS_MEM_POOL)
-	CLanGroupThread(bool setTlsMemPool, size_t tlsMemPoolUnitCnt, size_t tlsMemPoolCapacity)
-		: m_SetTlsMemPoolFlag(setTlsMemPool), m_TlsMemPoolUnitCnt(tlsMemPoolUnitCnt), m_TlsMemPoolCapacity(tlsMemPoolCapacity)
-#else 
-	CLanGroupThread()
-#endif
-	{
-#if defined(RECV_BUFF_LIST)
-		m_RecvQueueSync.accessCnt = 0;
-		m_RecvQueueSync.blocked = 0;
-		m_RecvQueueSyncTemp.accessCnt = 0;
-		m_RecvQueueSyncTemp.blocked = 0;
-#endif
-		m_RecvEvent = CreateEvent(NULL, false, false, NULL);
-		m_SessionGroupThreadStopEvent = CreateEvent(NULL, false, false, NULL);
-		m_SessionGroupThread = (HANDLE)_beginthreadex(NULL, 0, SessionGroupThreadFunc, this, 0, NULL);
-	}
-	~CLanGroupThread() {
-		SetEvent(m_SessionGroupThreadStopEvent);
-		WaitForSingleObject(m_SessionGroupThread, INFINITE);
-	}
-
-#if defined(ALLOC_BY_TLS_MEM_POOL)
-	void InitGroupThread(CLanGroupServer* clanGroupServer, GroupID groupID, TlsMemPoolManager<JBuffer>* serialBuffPoolMgr) {
-		m_ClanGroupServer = clanGroupServer;
-		m_GroupID = groupID;
-		m_SerialBuffPoolMgr = serialBuffPoolMgr;
-	}
-#else
-	void InitGroupThread(CLanGroupServer* clanGroupServer, GroupID groupID) {
-		m_ClanGroupServer = clanGroupServer;
-		m_GroupID = groupID;
-	}
-#endif
-
-	void PushRecvBuff(stSessionRecvBuff& recvBuff) {
-#if defined(RECV_BUFF_QUEUE)
-		m_RecvQueueMtx.lock();
-		m_RecvQueue.push(recvBuff);
-		m_temp_PushCnt++;
-		m_RecvQueueMtx.unlock();
-
-		SetEvent(m_RecvEvent);
-#elif defined(RECV_BUFF_LIST)
-		stRecvQueueSync cmp, exg;
-		cmp = exg = m_RecvQueueSync;
-		cmp.blocked = 0;
-		exg.blocked = 0;
-		exg.accessCnt += 1;
-		do {
-			UINT ret = InterlockedCompareExchange((UINT*)&m_RecvQueueSync, *(UINT*)&exg, *(UINT*)&cmp);
-			stRecvQueueSync* retSync = (stRecvQueueSync*)&ret;
-			if (ret == *(UINT*)&cmp) {
-				m_RecvQueueMtx.lock();
-				m_RecvQueue.push_back(recvBuff);
-				m_temp_PushCnt++;
-				m_RecvQueueMtx.unlock();
-				InterlockedDecrement((UINT*)&m_RecvQueueSync);
-				break;
-			}
-			else if (retSync->blocked == 0) {
-				continue;
-			}
-			else {	// retSync->blocked == 1
-				bool breakFlag = false;
-				do {
-					cmp = exg = m_RecvQueueSyncTemp;
-					cmp.blocked = 0;
-					exg.blocked = 0;
-					exg.accessCnt += 1;
-					ret = InterlockedCompareExchange((UINT*)&m_RecvQueueSyncTemp, *(UINT*)&exg, *(UINT*)&cmp);
-					stRecvQueueSync* retSync = (stRecvQueueSync*)&ret;
-					if (ret == *(UINT*)&cmp) {
-						m_RecvQueueMtx.lock();
-						m_RecvQueueTemp.push_back(recvBuff);
-						m_temp_PushCnt++;
-						m_RecvQueueMtx.unlock();
-						InterlockedDecrement((UINT*)&m_RecvQueueSyncTemp);
-						breakFlag = true;
-						break;
-					}
-					else if (retSync->blocked == 0) {
-						continue;
-					}
-					else {
-						break;
-					}
-				} while (true);
-
-				if (breakFlag) {
-					break;
-				}
-			}
-		} while (true);
-		
-		SetEvent(m_RecvEvent);
-#endif
-	}
-
-private:
-	// 이벤트를 받고, 메시지를 읽어 OnRecv 호출
-	static UINT __stdcall SessionGroupThreadFunc(void* arg);
-
-protected:
-#if defined(ALLOC_BY_TLS_MEM_POOL)
-	JBuffer* GetSerialSendBuff();
-#endif
-
-	void Disconnect(uint64 sessionID);
-	bool SendPacket(uint64 sessionID, JBuffer* sendDataPtr);
-	void ForwardSessionGroup(SessionID sessionID, GroupID to);
-	void PostMsgToThreadGroup(GroupID groupID, JBuffer& msg);
-
-	void Encode(BYTE randKey, USHORT payloadLen, BYTE& checkSum, BYTE* payloads);
-	bool Decode(BYTE randKey, USHORT payloadLen, BYTE checkSum, BYTE* payloads);
-
-protected:
-	virtual void OnStart() {};
-	virtual void OnRecv(SessionID sessionID, JBuffer& recvData) {};
-	virtual void OnStop() {};
-};
-
-
+class CLanGroupThread;
 
 class CLanGroupServer : public CLanServer
 {
@@ -204,7 +43,7 @@ private:
 	std::unordered_map<SessionID, GroupID>	m_SessionGroupID;
 	SRWLOCK									m_SessionGroupIDSrwLock;
 	// 그룹 별 이벤트
-	std::map<GroupID, CLanGroupThread*>	m_GroupThreads;
+	std::map<GroupID, CLanGroupThread*>		m_GroupThreads;
 
 public:
 	// 그룹 생성 (그룹 식별자 반환, 라이브러리와 컨텐츠는 그룹 식별자를 통해 식별)
@@ -227,10 +66,153 @@ protected:
 	// ex) DB로부터 그룹을 식별한다.
 	virtual void OnClientJoin(SessionID sessionID) {};
 	virtual void OnClientLeave(SessionID sessionID) {};
-	virtual UINT RecvData(JBuffer& recvBuff, JBuffer& dest) = 0;
+	//virtual UINT RecvData(JBuffer& recvBuff, JBuffer& dest) = 0;
 
 private:
 	// 세션 그룹을 분류함
 	virtual void OnRecv(SessionID sessionID, JBuffer& recvBuff);
 };
 
+class CLanGroupThread {
+private:
+#if !defined(POLLING_SESSION_MESSAGE_QUEUE)
+#if defined(RECV_BUFF_QUEUE)
+	std::queue<stSessionRecvBuff>	m_RecvQueue;
+#elif defined(RECV_BUFF_LIST)
+	std::list<stSessionRecvBuff>	m_RecvQueue;
+	std::list<stSessionRecvBuff>	m_RecvQueueTemp;
+	struct stRecvQueueSync {
+		UINT accessCnt : 24;
+		UINT blocked : 8;
+	};
+	stRecvQueueSync					m_RecvQueueSync;
+	stRecvQueueSync					m_RecvQueueSyncTemp;
+#endif	// RECV_BUFF_QUEUE, RECV_BUFF_LIST
+#endif	// POLLING_SESSION_MESSAGE_QUEUE
+
+#if defined(SETEVENT_RECEIVE_EVENT)
+	HANDLE							m_RecvEvent;
+	HANDLE							m_SessionGroupThreadStopEvent;
+#elif defined(POLLING_RECEIVE_EVENT)
+	BOOL							m_SessionGroupThreadStopFlag = false;
+#elif defined(POLLING_SESSION_MESSAGE_QUEUE)
+	using SessionQueueMap = std::map<SessionID, std::pair<std::queue<std::shared_ptr<JBuffer>>, CRITICAL_SECTION*>>;
+	SessionQueueMap	m_SessionMsgQueueMap;
+	SRWLOCK							m_SessionMsgQueueSRWLock;
+	BOOL							m_SessionGroupThreadStopFlag = false;
+#endif
+
+	UINT							m_temp_PushCnt = 0;
+	UINT							m_temp_PopCnt = 0;
+	std::mutex						m_RecvQueueMtx;
+
+	HANDLE							m_SessionGroupThread;
+
+	CLanGroupServer* m_ClanGroupServer;
+
+protected:
+	GroupID							m_GroupID;
+
+#if defined(ALLOC_BY_TLS_MEM_POOL)
+	TlsMemPoolManager<JBuffer>* m_SerialBuffPoolMgr;
+	bool						m_SetTlsMemPoolFlag;
+	size_t						m_TlsMemPoolUnitCnt;
+	size_t						m_TlsMemPoolCapacity;
+#endif
+
+public:
+#if defined(ALLOC_BY_TLS_MEM_POOL)
+	CLanGroupThread(bool setTlsMemPool, size_t tlsMemPoolUnitCnt, size_t tlsMemPoolCapacity)
+		: m_SetTlsMemPoolFlag(setTlsMemPool), m_TlsMemPoolUnitCnt(tlsMemPoolUnitCnt), m_TlsMemPoolCapacity(tlsMemPoolCapacity)
+#else 
+	CLanGroupThread()
+#endif
+	{
+#if !defined(POLLING_SESSION_MESSAGE_QUEUE)
+#if defined(RECV_BUFF_LIST)
+		m_RecvQueueSync.accessCnt = 0;
+		m_RecvQueueSync.blocked = 0;
+		m_RecvQueueSyncTemp.accessCnt = 0;
+		m_RecvQueueSyncTemp.blocked = 0;
+#endif	// RECV_BUFF_LIST
+#endif	// POLLING_SESSION_MESSAGE_QUEUE
+
+#if defined(SETEVENT_RECEIVE_EVENT)
+		m_RecvEvent = CreateEvent(NULL, false, false, NULL);
+		m_SessionGroupThreadStopEvent = CreateEvent(NULL, false, false, NULL);
+#endif
+		m_SessionGroupThread = (HANDLE)_beginthreadex(NULL, 0, SessionGroupThreadFunc, this, 0, NULL);
+	}
+	~CLanGroupThread() {
+#if defined(SETEVENT_RECEIVE_EVENT)
+		SetEvent(m_SessionGroupThreadStopEvent);
+#elif defined(POLLING_RECEIVE_EVENT)
+		m_SessionGroupThreadStopFlag = true;
+#endif
+		WaitForSingleObject(m_SessionGroupThread, INFINITE);
+	}
+#if defined(ALLOC_BY_TLS_MEM_POOL)
+	void InitGroupThread(CLanGroupServer* clanGroupServer, GroupID groupID, TlsMemPoolManager<JBuffer>* serialBuffPoolMgr) {
+		m_ClanGroupServer = clanGroupServer;
+		m_GroupID = groupID;
+		m_SerialBuffPoolMgr = serialBuffPoolMgr;
+	}
+#else
+	void InitGroupThread(CLanGroupServer* clanGroupServer, GroupID groupID) {
+		m_ClanGroupServer = clanGroupServer;
+		m_GroupID = groupID;
+	}
+#endif
+
+	void PushRecvBuff(stSessionRecvBuff& recvBuff);
+
+private:
+	// 이벤트를 받고, 메시지를 읽어 OnRecv 호출
+	static UINT __stdcall SessionGroupThreadFunc(void* arg);
+
+protected:
+#if defined(ALLOC_BY_TLS_MEM_POOL)
+	inline JBuffer* AllocSerialBuff() {
+		return m_ClanGroupServer->AllocSerialBuff();
+	}
+	inline JBuffer* AllocSerialBuff(const std::string& log) {
+		return m_ClanGroupServer->AllocSerialBuff(log);
+	}
+	inline JBuffer* AllocSerialSendBuff(USHORT length) {
+		return m_ClanGroupServer->AllocSerialSendBuff(length);
+	}
+	inline JBuffer* AllocSerialSendBuff(USHORT length, const std::string& log) {
+		return m_ClanGroupServer->AllocSerialSendBuff(length, log);
+	}
+	inline void FreeSerialBuff(JBuffer* buff) {
+		m_ClanGroupServer->FreeSerialBuff(buff);
+	}
+	inline void FreeSerialBuff(JBuffer* buff, const std::string& log) {
+		m_ClanGroupServer->FreeSerialBuff(buff, log);
+	}
+	inline void AddRefSerialBuff(JBuffer* buff) {
+		m_ClanGroupServer->AddRefSerialBuff(buff);
+	}
+	inline void AddRefSerialBuff(JBuffer* buff, const std::string& log) {
+		m_ClanGroupServer->AddRefSerialBuff(buff, log);
+	}
+#endif
+#if defined(CALCULATE_TRANSACTION_PER_SECOND)
+	inline void IncrementRecvTransaction(LONG cnt = 1) {
+		m_ClanGroupServer->IncrementRecvTransaction(cnt);
+	}
+#endif
+
+	void Disconnect(uint64 sessionID);
+	bool SendPacket(uint64 sessionID, JBuffer* sendDataPtr);
+	void ForwardSessionGroup(SessionID sessionID, GroupID to);
+	void PostMsgToThreadGroup(GroupID groupID, JBuffer& msg);
+
+	//void Encode(BYTE randKey, USHORT payloadLen, BYTE& checkSum, BYTE* payloads);
+	//bool Decode(BYTE randKey, USHORT payloadLen, BYTE checkSum, BYTE* payloads);
+
+protected:
+	virtual void OnStart() {};
+	virtual void OnMessage(SessionID sessionID, JBuffer& recvData) = 0;
+	virtual void OnStop() {};
+};
