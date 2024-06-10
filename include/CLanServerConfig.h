2aa5c9909cd0620fde157562a746bec3e726fc12
JinHur_Server <jinh2352@gmail.com>
2024-06-10 15:17:18 +0900

    Tls 메모리 풀의 직렬화 버퍼 객체 내부 버퍼 사이즈 지정 가능, 직렬화 버퍼 고정 크기 지정

--- include/CLanServerConfig.h
+++ include/CLanServerConfig.h
@@ -33,6 +33,7 @@
 // 2. SendPacket, 송신 직렬화 버퍼 포인터 전달
 #define SEND_RECV_RING_BUFF_SERIALIZATION_MODE
 #if defined(SEND_RECV_RING_BUFF_SERIALIZATION_MODE)
+#define DEFAULT_SERIAL_BUFFER_SIZE			1000
 #define WSABUF_ARRAY_DEFAULT_SIZE			1000
 #endif
 
@@ -46,11 +47,6 @@
 // 메모리 풀
 ////////////////////////////////////////////////////
 #define ALLOC_BY_TLS_MEM_POOL
-#if defined(ALLOC_BY_TLS_MEM_POOL)
-#define TLS_MEM_POOL_DEFAULT_UNIT_CNT		100
-#define TLS_MEM_POOL_DEFAULT_CAPACITY		100
-#define TLS_MEM_POOL_DEFAULT_SURPLUS_SIZE	100
-#endif
 
 ////////////////////////////////////////////////////
 // 수신 버퍼링
@@ -67,7 +63,7 @@
 ////////////////////////////////////////////////////
 // TPS(Transaction Per Second)
 ////////////////////////////////////////////////////
-#define CALCULATE_TRANSACTION_PER_SECOND
+//#define CALCULATE_TRANSACTION_PER_SECOND
 #if defined(CALCULATE_TRANSACTION_PER_SECOND)
 #define		ACCEPT_TRANSACTION		0
 #define		RECV_TRANSACTION		1
